decace194dea4ef0373b12ddb849539c1cbe7011
Adam Luchini and George Olson <adam+george@>
2016-09-26 15:08:15 -0700

    Add foursquare api functionality

---
+++ src/FourSquareApp/Controllers/HomeController.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using FourSquareApp.Models;
+
+namespace FourSquareApp.Controllers
+{
+    public class HomeController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+        public IActionResult GetVenues()
+        {
+            var allVenues = Venue.GetVenues();
+            return View(allVenues);
+        }
+        public IActionResult SendVenue()
+        {
+            return View();
+        }
+        [HttpPost]
+        public IActionResult SendVenue(Venue newVenue)
+        {
+            newVenue.Send();
+            return RedirectToAction("Index");
+        }
+    }
+}
---
+++ src/FourSquareApp/Models/Venue.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+using RestSharp;
+using RestSharp.Authenticators;
+using Newtonsoft.Json;
+using Newtonsoft.Json.Linq;
+
+namespace FourSquareApp.Models
+{
+    // urlhttp://json2csharp.com/
+
+    public class Venue
+    {
+        public string Name { get; set; }
+        public string Url { get; set; }
+        public virtual Location Location { get; set; }
+        public static List<Venue> GetVenues()
+        {
+            
+            var client = new RestClient("https://api.foursquare.com/v2/");
+            var request = new RestRequest("venues/search?ll=40.7,-74&client_id=XTHMGYYKXJIEAQCYGSDRZIW5W3CNSJRES0CDBDHCSUNALGFU&client_secret=ZIZVKXIGNL3UIYOEUKTFICX5JHHQIQJCF4G5NSUWWTH4ZNV5&v=20160926", Method.GET);
+            var response = new RestResponse();
+            Task.Run(async () =>
+            {
+                response = await GetResponseContentAsync(client, request) as RestResponse;
+            }).Wait();
+            JObject jsonResponse = JsonConvert.DeserializeObject<JObject>(response.Content);
+            var venuesList = JsonConvert.DeserializeObject<List<Venue>>(jsonResponse["response"]["venues"].ToString());
+
+            
+            return venuesList;
+
+        }
+        public void Send()
+        {
+            var client = new RestClient("https://api.foursquare.com/v2/");
+            var request = new RestRequest("venues/search?ll=40.7,-74&client_id=XTHMGYYKXJIEAQCYGSDRZIW5W3CNSJRES0CDBDHCSUNALGFU&client_secret=ZIZVKXIGNL3UIYOEUKTFICX5JHHQIQJCF4G5NSUWWTH4ZNV5&v=20160926", Method.POST);
+            request.AddParameter("Name", Name);
+            client.ExecuteAsync(request, response => {
+                Console.WriteLine(response.Content);
+            });
+        }
+        public static Task<IRestResponse> GetResponseContentAsync(RestClient theClient, RestRequest theRequest)
+        {
+            var tcs = new TaskCompletionSource<IRestResponse>();
+            theClient.ExecuteAsync(theRequest, response =>
+            {
+                tcs.SetResult(response);
+            });
+            return tcs.Task;
+        }
+    }
+}
